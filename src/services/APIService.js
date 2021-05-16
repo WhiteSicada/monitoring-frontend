a3942309fb879b82726ab7492ab6bdcca2311cbc
WhiteSicada <dino_villa@hotmail.fr>
2021-05-16 21:50:26 +0100

    add endpoints axios methods

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -16,11 +16,26 @@ const updateAPI = (id, api) => {
 	return customAxios.put(`/api/${id}`, api);
 };
 
+const addEndpointsToApi = (id, endpoints) => {
+	return customAxios.put(`/api/${id}/addEndpoints`, endpoints);
+};
+
+const removeEndpointsToApi = (id, endpoints) => {
+	return customAxios.put(`/api/${id}/removeEndpoints`, endpoints);
+};
+
+const updateEndpointsForApi = (id, endpoints) => {
+	return customAxios.put(`/api/${id}/updateEndpoints`, endpoints);
+};
+
 const APIService = {
 	createAPI,
 	getAPIs,
 	deleteAPI,
 	updateAPI,
+	addEndpointsToApi,
+	removeEndpointsToApi,
+	updateEndpointsForApi,
 };
 
 export default APIService;
